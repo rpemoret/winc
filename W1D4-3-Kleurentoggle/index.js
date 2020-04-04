0414a9801c7d313ccb81030e89da3e9fe2e9b5c7
René Moret <rpemoret@hotmail.com>
2020-04-04 18:06:41 +0200

    adds

--- W1D4-3-Kleurentoggle/index.js
+++
@@ -1,9 +0,0 @@
-const content = document.getElementById("newbutton");
-
-const button = document.getElementById("button");
-
-button.onclick = function {
-
-    if (content.className == "open") { content.className = "" }
-    else { content.className == "open" }
-}
